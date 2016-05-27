4d183faff6014f50ac177ac575db0bd0afa3f6cf
Naser Afsar <nafsar@gmail.com>
2016-05-26 20:38:43 -0600

    add files

---
+++ app.js
@@ -0,0 +1,36 @@
+(function (angular) {
+  'use strict';
+  angular
+    .module("myApp", [])
+    .service("listService", listService)
+    .controller("ListControl", ListControl)
+    .directive("listDisplay", listDisplay);
+
+  function ListControl($http, listService) {
+    var lstblk = this;
+    listService.then(function (data) {    
+      lstblk.listItems = data;
+    });
+  };
+
+  function listService($http) {
+    return $http.get("./lib/data.json").then(function (response) {
+      return response.data;
+    }, function (rejected) {
+      console.log("No data found!");
+      window.alert("No data found!\n\n" + JSON.stringify(rejected.data));
+    });
+  };
+ 
+  function listDisplay() {
+    var directive = {
+      restrict: "EA",
+      templateUrl: "./display.html"
+    }
+    return directive;
+  };
+})(window.angular);
+
+
+
+
